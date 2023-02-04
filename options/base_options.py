33e27947fe2233f547fb448f08db533045a7f3ce
kaylahoo <790970276@qq.com>
2023-02-04 21:51:15 +0800

    MED

--- options/base_options.py
+++ options/base_options.py
@@ -22,7 +22,7 @@ class BaseOptions():
         parser.add_argument('--ngf', type=int, default=64, help='# of gen filters in first conv layer')
         parser.add_argument('--ndf', type=int, default=64, help='# of discrim filters in first conv layer')
         parser.add_argument('--n_layers_D', type=int, default=3, help='only used if which_model_netD==n_layers')
-        parser.add_argument('--gpu_ids', type=str, default='0,1', help='gpu ids: e.g. 0  0,1,2, 0,2')
+        parser.add_argument('--gpu_ids', type=str, default='1', help='gpu ids: e.g. 0  0,1,2, 0,2')
         parser.add_argument('--model', type=str, default='training1', help='set the names of current training process')
         parser.add_argument('--nThreads', default=2, type=int, help='# threads for loading data')
         parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints/Mutual-Encoder-Decoder', help='models are saved here')
