9b8c4939482c02d1463bf6fd30a7f0aff4fdbd8d
kaylahoo <790970276@qq.com>
2023-02-05 17:29:18 +0800

    MED

--- test.py
+++ test.py
@@ -29,8 +29,8 @@ if __name__ == "__main__":
     opt = TestOptions().parse()
     model = create_model(opt)
     model.netEN.module.load_state_dict(torch.load("/home/lab265/lab265/liulu/MED/checkpoints/Mutual-Encoder-Decoder/37_net_EN.pth")['net'])
-    model.netDE.module.load_state_dict(torch.load("/home/lab265/lab265/liulu/MED/checkpoints/Mutual-Encoder-Decoder/37_net_DE.pkl")['net'])
-    model.netMEDFE.module.load_state_dict(torch.load("/home/lab265/lab265/liulu/MED/checkpoints/Mutual-Encoder-Decoder/37_net_MEDEF.pkl")['net'])
+    model.netDE.module.load_state_dict(torch.load("/home/lab265/lab265/liulu/MED/checkpoints/Mutual-Encoder-Decoder/37_net_DE.pth")['net'])
+    model.netMEDFE.module.load_state_dict(torch.load("/home/lab265/lab265/liulu/MED/checkpoints/Mutual-Encoder-Decoder/37_net_MEDEF.pth")['net'])
     results_dir = r'./result'
     if not os.path.exists( results_dir):
         os.mkdir(results_dir)
